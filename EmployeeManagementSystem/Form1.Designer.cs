0b9829fea5b5de29399992823d4406692a5eebfd
jungdaekim <tojdkim@gmail.com>
2024-09-28 11:07:34 +0900

    Add files via upload

---
+++ EmployeeManagementSystem/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace EmployeeManagementSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button1 = new Button();
+            label2 = new Label();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            checkBox1 = new CheckBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.BlueViolet;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(342, 486);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Navy;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 64);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.ForeColor = SystemColors.HighlightText;
+            button1.Location = new Point(71, 404);
+            button1.Margin = new Padding(1);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 29);
+            button1.TabIndex = 2;
+            button1.Text = "계정 등록";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(119, 383);
+            label2.Name = "label2";
+            label2.Size = new Size(104, 20);
+            label2.TabIndex = 1;
+            label2.Text = "계정 등록하기";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(57, 159);
+            label1.Name = "label1";
+            label1.Size = new Size(228, 41);
+            label1.TabIndex = 0;
+            label1.Text = "인적관리시스템";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Navy;
+            button2.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ControlLightLight;
+            button2.Location = new Point(425, 362);
+            button2.Name = "button2";
+            button2.Size = new Size(124, 64);
+            button2.TabIndex = 7;
+            button2.Text = "접속 시작";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(791, 5);
+            button3.Name = "button3";
+            button3.Size = new Size(41, 29);
+            button3.TabIndex = 9;
+            button3.Text = "X";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(425, 95);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 28);
+            label3.TabIndex = 2;
+            label3.Text = "접속 계정";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(425, 178);
+            label4.Name = "label4";
+            label4.Size = new Size(124, 20);
+            label4.TabIndex = 3;
+            label4.Text = "사용자 식별 기호";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(425, 253);
+            label5.Name = "label5";
+            label5.Size = new Size(89, 20);
+            label5.TabIndex = 5;
+            label5.Text = "사용자 암호";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(425, 205);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(350, 34);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(425, 276);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(350, 34);
+            textBox2.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(632, 325);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(146, 24);
+            checkBox1.TabIndex = 8;
+            checkBox1.Text = "암호 글자 보이기";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(836, 486);
+            Controls.Add(button3);
+            Controls.Add(checkBox1);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private CheckBox checkBox1;
+
+    }
+}
---
+++ EmployeeManagementSystem/Form1.cs
@@ -0,0 +1,16 @@
+namespace EmployeeManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
